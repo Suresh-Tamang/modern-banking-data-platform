b55681545249c4b07af2227303fe3b57355453e2
Suresh-Tamang <sureshjimba3333@gmail.com>
2026-01-24 22:23:45 +0545

    managed

--- etl/src/config.py
+++ etl/src/config.py
@@ -1,5 +1,22 @@
-import yaml
+from __future__ import annotations
+import os, yaml 
+from dotenv import load_dotenv
+from pathlib import Path
+load_dotenv()
 
-with open("config/settings.yaml") as f:
-    settings = yaml.safe_load(f)
-    
+BASE_DIR = Path(__file__).resolve().parent.parent
+CONFIG_DIR = BASE_DIR / "config"/"settings.yaml"
+
+def load_settings() -> dict:
+    """Load configuration settings from a YAML file."""
+    with open(CONFIG_DIR, 'r') as f:
+        settings = yaml.safe_load(f)
+    return settings
+
+
+def env(key: str) -> str:
+    """Get environment variable or raise error if not found."""
+    value = os.getenv(key)
+    if value is None:
+        raise EnvironmentError(f"Environment variable '{key}' not found.")
+    return value
--- etl/src/logging_setup.py
+++ etl/src/logging_setup.py
@@ -1,9 +1,21 @@
 import logging
 import logging.config
 import yaml
+from pathlib import Path
 
-with open("config/logging.yaml") as f:
-    config = yaml.safe_load(f)
-    logging.config.dictConfig(config)
 
-logger = logging.getLogger(__name__)
+def setup_logging() -> None:
+    """Set up logging configuration from a YAML file."""
+    base_dir = Path(__file__).resolve().parent.parent
+    log_config_path = base_dir/"config"/"logging.yaml"
+    logs_dir = base_dir/"logs"
+    
+    #Create logs directory if it doenst exist make it
+    logs_dir.mkdir(exist_ok=True)
+    
+    with open(log_config_path, 'r') as f:
+        config = yaml.safe_load(f.read())
+        # update log file path to absolute
+        if 'file' in config.get('handlers', {}):
+            config['handlers']['file']['filename']=str(logs_dir/'etl.log')
+        logging.config.dictConfig(config)
