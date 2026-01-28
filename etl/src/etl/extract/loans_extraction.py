44d5a9dabc4da82ec6cbc719693effc5b4f626bc
Suresh-Tamang <sureshjimba3333@gmail.com>
2026-01-28 21:32:10 +0545

    ..

--- etl/src/etl/extract/loans_extraction.py
+++ etl/src/etl/extract/loans_extraction.py
@@ -6,7 +6,7 @@ from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_excep
 log = logging.getLogger(__name__)
 
 
-class AccountClient:
+class LoanClient:
     def __init__(self, base_url: str, api_key: str = None, rate_limit_per_sec: float = 5.0):
         self.base_url = base_url.rstrip("/")
         self.session = requests.Session()
